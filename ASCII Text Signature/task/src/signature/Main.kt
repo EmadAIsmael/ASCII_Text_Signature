cfadf465c05571be1b81f0eba892f5b64c404a0f
Emad A. Ismael <EmadAIsmael@yahoo.com>
2020-04-02 04:26:31 +0200

    Thursday: 2020/04/02 04:25 stage #4

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,149 +1,138 @@
 package signature
 
+import java.io.File
 import java.util.Scanner
 import kotlin.math.abs
 import kotlin.math.max
 
-enum class Character(val character: Char, val scanLines: Array<String>) {
-    A('A', arrayOf("____", "|__|", "|  |")),
-    B('B', arrayOf("___ ", "|__]", "|__]")),
-    C('C', arrayOf("____", "|   ", "|___")),
-    D('D', arrayOf("___ ", "|  \\", "|__/")),
-    E('E', arrayOf("____", "|___", "|___")),
-    F('F', arrayOf("____", "|___", "|   ")),
-    G('G', arrayOf("____", "| __", "|__]")),
-    H('H', arrayOf("_  _", "|__|", "|  |")),
-    I('I', arrayOf("_", "|", "|")),
-    J('J', arrayOf(" _", " |", "_|")),
-    K('K', arrayOf("_  _", "|_/ ", "| \\_")),
-    L('L', arrayOf("_   ", "|   ", "|___")),
-    M('M', arrayOf("_  _", "|\\/|", "|  |")),
-    N('N', arrayOf("_  _", "|\\ |", "| \\|")),
-    O('O', arrayOf("____", "|  |", "|__|")),
-    P('P', arrayOf("___ ", "|__]", "|   ")),
-    Q('Q', arrayOf("____", "|  |", "|_\\|")),
-    R('R', arrayOf("____", "|__/", "|  \\")),
-    S('S', arrayOf("____", "[__ ", "___]")),
-    T('T', arrayOf("___", " | ", " | ")),
-    U('U', arrayOf("_  _", "|  |", "|__|")),
-    V('V', arrayOf("_  _", "|  |", " \\/ ")),
-    W('W', arrayOf("_ _ _", "| | |", "|_|_|")),
-    X('X', arrayOf("_  _", " \\/ ", "_/\\_")),
-    Y('Y', arrayOf("_   _", " \\_/ ", "  |  ")),
-    Z('Z', arrayOf("___ ", "  / ", " /__"));
 
-    companion object {
+class Character(val charWidth: Int, val repr: Array<String>)
 
-        fun charWidth(c: Char): Int {
-            return when (c) {
-                'I' -> 1
-                'J' -> 2
-                'T' -> 3
-                'W' -> 5
-                'Y' -> 5
-                else -> 4
-            }
+
+open class Font(private val fontFile: String) {
+
+    private val font = mutableMapOf<Char, Character>()
+    private var charSize = 0
+    private var fontSize: Int = 0
+
+    fun read() {
+        val scanner = Scanner(File(fontFile))
+
+        this.charSize = scanner.nextInt()        // char height/num lines
+        this.fontSize = scanner.nextInt()        // num chars per font
+
+        for (i in 0 until fontSize) {
+
+            val c = scanner.next().first()      // char symbol
+            val charWidth = scanner.nextInt()   // char width
+
+            scanner.nextLine()                  // dummy read to skip current line
+            val repr = Array(charSize) { "" }
+            for (j in 0 until charSize)
+                repr[j] = scanner.nextLine()
+
+            val char = Character(charWidth, repr)
+            font[c] = char
         }
+        // Add space character
+        val spaceRepr = Array(charSize) { " ".repeat(font['a']?.charWidth ?: 0) }
 
+        font[' '] = Character(
+            font['a']?.charWidth ?: 0,
+            spaceRepr
+        )
     }
-}
 
-fun printScanLine(c: Char, i: Int) {
-    if (c == ' ') {
-        // four character for space + 1 more for right seperators.
-        print(" ".repeat(5))
-        return
-    }
-    for (enum in Character.values())
-        if (c == enum.character) {
-            print("${enum.scanLines[i]} ")
-            break
+    private fun printScanLine(i: Int, c: Char) = print("${font[c]?.repr?.get(i)}")
+
+    fun printText(text: String, prefix: String, suffix: String) {
+        for (i in 0 until charSize) {
+            print(prefix)
+            for (c in text)
+                printScanLine(i, c)
+
+            print(suffix)
+            println()
         }
-}
-
-fun printName(name: String, prefix: String, suffix: String) {
-    for (i in 0 until 3) {
-        print("$prefix  ")
-        for (c in name) {
-            printScanLine(c, i)
-        }
-        print(" $suffix")
-        println()
     }
-}
 
-fun scanLineCharLen(name: String): Int {
-    var sum = 0
-    for (c in name)
-        sum += if (c == ' ')
-            4
-        else
-            Character.charWidth(c)
-    return sum
+    fun scanLineCharLen(text: String): Int {
+        var sum = 0
+        for (c in text)
+            sum += font[c]?.charWidth ?: 0
+        return sum
+    }
+
+    /**
+     * @param lineLen: length of printing line
+     * @param strLen: length of string to center within 'lineLen'
+     */
+    fun center(lineLen: Int, strLen: Int): MutableMap<String, Int> {
+        val diff = abs(lineLen - strLen)
+        val prefix = diff / 2
+        val suffix = prefix + if (diff % 2 == 0) 0 else 1
+
+        return mutableMapOf("prefix" to prefix, "suffix" to suffix)
+    }
 }
 
 fun nameTag(name: String, status: String) {
-    val top = "*"
-    val bottom = "*"
+
+    val mediumFontFile =
+        "/home/emad/Documents/Development/LEARN/LearnKotlin/hyperskill/kotlin/ASCII Text Signature/ASCII Text Signature/task/src/signature/medium.txt"
+    val romanFontFile =
+        "/home/emad/Documents/Development/LEARN/LearnKotlin/hyperskill/kotlin/ASCII Text Signature/ASCII Text Signature/task/src/signature/roman.txt"
+    val medium = Font(mediumFontFile)
+    val roman = Font(romanFontFile)
+
+    medium.read()
+    roman.read()
+
+    val top = "8"
+    val bottom = "8"
     val space = " "
 
-    // star + 2 spaces +
-    // width(name scan lines) characters +
-    // len(name) spaces +
-    // 2 spaces + star
-    val spaces = name.length - 1                 // one space after each character
-    // star + 2 spaces prefix,
-    // 1 space + star suffix
-    // scanLine contains a trailing space
-    val scanLineLen = 3 + scanLineCharLen(name) + spaces + 3
+    val nameLineLen = roman.scanLineCharLen(name)
+    val statusLineLen = medium.scanLineCharLen(status)
 
-    // prefix: star + 2 spaces = 3
-    // suffix: two spaces + star = 3
-    val statusLineLen = 3 + status.length + 3
+    val outputLineLen =
+        if (nameLineLen > statusLineLen)
+            nameLineLen
+        else statusLineLen
 
-    val len: Int
-    val scanLinePrefix: String
-    val scanLineSuffix: String
+    val nameLinePrefix: String
+    val nameLineSuffix: String
     val statusLinePrefix: String
     val statusLineSuffix: String
 
-    val lenDiff = abs(statusLineLen - scanLineLen)
-    val prefixHalfLen = lenDiff / 2
-    val suffixHalfLen = prefixHalfLen + if (lenDiff % 2 == 0) 0 else +1
+    val namePaddingLen = roman.center(outputLineLen, nameLineLen)
+    val statusPaddingLen = medium.center(outputLineLen, statusLineLen)
 
-    if (scanLineLen > statusLineLen) {
-        len = scanLineLen
-        scanLinePrefix = "*"
-        scanLineSuffix = "*"
+    nameLinePrefix = "88  " + space.repeat(max(0, namePaddingLen["prefix"]!!))
+    nameLineSuffix = space.repeat(max(0, namePaddingLen["suffix"]!!)) + "  88"
 
-        statusLinePrefix = "*  " + space.repeat(max(0, prefixHalfLen))
-        statusLineSuffix = space.repeat(max(0, suffixHalfLen)) + "  *"
-    } else {
-        len = statusLineLen
-        scanLinePrefix = "*" + space.repeat(max(0, prefixHalfLen))
-        scanLineSuffix = space.repeat(max(0, suffixHalfLen)) + "*"
+    statusLinePrefix = "88  " + space.repeat(max(0, statusPaddingLen["prefix"]!!))
+    statusLineSuffix = space.repeat(max(0, statusPaddingLen["suffix"]!!))+ "  88"
 
-        statusLinePrefix = "*  "
-        statusLineSuffix = "  *"
-    }
-
-    val statusLine = "$statusLinePrefix$status$statusLineSuffix"
-
-    println(top.repeat(len))
-
-    printName(name, scanLinePrefix, scanLineSuffix)
-
-    println(statusLine)
-
-    println(bottom.repeat(len))
+    // two '8' chars for border + two spaces
+    // 2 spaces + two '8' chars
+    // total extra chars = 8
+    val extraBorder = 8
+    println(top.repeat(outputLineLen + extraBorder))
+    roman.printText(name, nameLinePrefix, nameLineSuffix)
+    medium.printText(status, statusLinePrefix, statusLineSuffix)
+    println(bottom.repeat(outputLineLen + extraBorder))
 }
 
 fun main() {
 
     val scanner = Scanner(System.`in`)
 
+    print("Enter name and surname: ")
     val name = scanner.nextLine()
+
+    print("Enter person's status: ")
     val status = scanner.nextLine()
 
-    nameTag(name.toUpperCase(), status)
+    nameTag(name, status)
 }
